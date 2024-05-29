d185768592405d80064258e00d8eb8e1200fb3d0
Jeph2001 <josephmaniragaba9@gmail.com>
2024-05-29 13:26:59 +0200

    Added serveless functions

---
+++ api/greetings.js
@@ -0,0 +1,11 @@
+// GET requests to return "Hello, world!"
+export const onRequestGet = () => {
+    return new Response("Hello, world!")
+  }
+
+
+// POST requests  with a JSON-encoded body would return "Hello, <name>!"
+export const onRequestPost = async ({ request }) => {
+    const { name } = await request.json()
+    return new Response(`Hello, ${name}!`)
+  }
